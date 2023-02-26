76a019a16d38957105988fb2e64e6f38ff62b504
강준 <keisoft.kjun@gmail.com>
2023-02-26 21:11:43 +0900

    기본 프로젝트 WPF.AppTest 구성

---
+++ WPF.AppTest/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF.AppTest
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private ObservableCollection<string> items;
+
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                if (this.items == null)
+                {
+                    this.items = new ObservableCollection<string>();
+                }
+
+                return this.items;
+            }
+            set
+            {
+                this.items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+    }
+}
